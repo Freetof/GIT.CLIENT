c3c4f497f55fc4c2de1b0a69f9c24519696186ca
Freetof <freetof.usa@gmail.com>
2019-05-12 22:42:25 -0600

    FINAL SUBMISSION

--- src/main/java/com/chat/client/Client.java
+++ src/main/java/com/chat/client/Client.java
@@ -1,80 +1,54 @@
 package main.java.com.chat.client;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
+import java.time.LocalDateTime;
 
-public class Client {
-    public Client(String name) {
-        this.name = name;
-    }
+public class Client extends JFrame implements ActionListener {
+    private final int ServerPort = 8888;
+    private String name;
+    private boolean isActive = true;
 
-    final JPanel comboPanel = new JPanel();
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
 
-    private static String name = "";
+    private JTextField jtextFieldInput;
+    private JTextArea jtAreaOutput;
 
-    public static String getName() {
-        return name;
-    }
+    private JTextField jTextField;
 
-    final static int ServerPort = 8888;
 
-    public static void main(String args[]) throws UnknownHostException, IOException {
-
-        if (name.equals("")) {
-            System.out.println("enter the name:");
-            Scanner sc = new Scanner(System.in);
-            name = sc.next();
-        }
-        System.out.println(getName() + " is your name for this session\n");
-        System.out.println("enter anything to confirm connection...");
-
-        Scanner scn = new Scanner(System.in);
-
+    public Client() throws Exception {
         // getting localhost ip
         InetAddress ip = InetAddress.getByName("localhost");
 
         // establish the connection
-        Socket s = new Socket(ip, ServerPort);
+        Socket socket = new Socket(ip, ServerPort);
 
         // obtaining input and out streams
-        DataInputStream dis = new DataInputStream(s.getInputStream());
-        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-        // sendMessage thread
-        Thread sendMessage = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Client client = new Client(name);
-                while (true) {
-                    // read the message to deliver.
-                    String msg = client.getName() + ":" + scn.nextLine();
-
-                    try {
-                        // write on the output stream
-                        dos.writeUTF(msg);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
+        createGui();
+        createModalWindow();
 
         // readMessage thread
         Thread readMessage = new Thread(new Runnable() {
             @Override
             public void run() {
 
-                while (true) {
+                while (isActive) {
                     try {
                         // read the message sent to this client
-                        String msg = dis.readUTF();
+                        String msg = dataInputStream.readUTF();
                         System.out.println(msg);
+                        addAreaFieldMessage(msg);
                     } catch (IOException e) {
 
                         e.printStackTrace();
@@ -82,9 +56,107 @@ public class Client {
                 }
             }
         });
-
-        sendMessage.start();
+        System.out.println("RUN");
         readMessage.start();
 
     }
+
+    private String getClientName() {
+        return name;
+    }
+
+    public void createGui() {
+        jtextFieldInput = new JTextField(100);
+
+        this.setTitle(getClientName());
+        jtextFieldInput.addActionListener(this);
+        jtAreaOutput = new JTextArea(50, 100);
+        jtAreaOutput.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(jtAreaOutput,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        GridBagLayout gridBag = new GridBagLayout();
+        Container contentPane = getContentPane();
+        contentPane.setLayout(gridBag);
+        GridBagConstraints gridCons1 = new GridBagConstraints();
+        gridCons1.gridwidth = GridBagConstraints.REMAINDER;
+        gridCons1.fill = GridBagConstraints.HORIZONTAL;
+        contentPane.add(jtextFieldInput, gridCons1);
+        GridBagConstraints gridCons2 = new GridBagConstraints();
+        gridCons2.weightx = 1.0;
+        gridCons2.weighty = 1.0;
+        contentPane.add(scrollPane, gridCons2);
+        this.pack();
+        this.addWindowListener(new WindowAdapter() {
+
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        String text = jtextFieldInput.getText();
+        addAreaFieldMessage(text);
+        jtextFieldInput.setText("");
+        sendMessage(text);
+    }
+
+    private void sendMessage(String text){
+        try {
+            // write on the output stream
+            dataOutputStream.writeUTF(getClientName() + ":" + text);
+        } catch (IOException e) {
+            e.printStackTrace();
+            isActive = false;
+        }
+    }
+
+    private void addAreaFieldMessage(String message){
+        jtAreaOutput.append(LocalDateTime.now() +" : "+ message + "\n");
+    }
+
+    private void createModalWindow() {
+        JDialog dialog = new JDialog(this, Dialog.ModalityType.APPLICATION_MODAL);
+        dialog.setBounds(132, 132, 300, 200);
+        dialog.addWindowListener(closeWindow);
+        JLabel label = new JLabel("Enter your name: ");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        jTextField = new JTextField(12);
+        JButton button = new JButton("OK");
+        button.setHorizontalAlignment(SwingConstants.CENTER);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                name = jTextField.getText();
+                Client.super.setTitle("Logged in as : " + name);
+                sendMessage("init_message");
+                dialog.setVisible(false);
+            }
+        });
+        Container contentPane = dialog.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        contentPane.add(label, BorderLayout.NORTH);
+        contentPane.add(jTextField, BorderLayout.CENTER);
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new FlowLayout());
+        jPanel.add(button);
+        contentPane.add(jPanel, BorderLayout.SOUTH);
+        dialog.setVisible(true);
+    }
+
+    public static void main(String args[]) throws IOException {
+        try {
+            new Client();
+        } catch (Exception ex) {
+            System.out.println("Unexpected error occurs");
+            ex.printStackTrace();
+        }
+    }
+
+    private static WindowListener closeWindow = new WindowAdapter() {
+        public void windowClosing(WindowEvent e) {
+            e.getWindow().dispose();
+        }
+    };
 } 
